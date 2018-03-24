89b8da6a69f6441cccdff8d291fc7cea397354cd
fschueler <fschueler@posteo.de>
2018-03-25 00:44:36 +0100

    add getOrElse and flatMap2

--- fp_scala/src/scala/fpinscala/exceptions/Option.scala
+++ fp_scala/src/scala/fpinscala/exceptions/Option.scala
@@ -1,10 +1,34 @@
 package fpinscala.exceptions
 
 trait Option[+A] {
+
+  // map can only "forward" None and Some
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(v) => Some(f(v))
   }
+
+  // flatMap can forward None but can also turn a Some into a None! (f can fail)
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(v) => f(v)
+  }
+
+  // extracts the value from a Some(v) or returns the default if the option is None
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
+
+  /* - map(f) returns an Option[Option[B]]
+   * - getOrElse checks if we have Some[Option[B]] or None
+   *   - in the case of Some[Option[B]] we extract the inner option
+   *     which can be Some[B] or None (depending on the result of f)
+   *   - in the case of None, we forward that 
+   */
+  def flatMap2[B](f: A => Option[B]): Option[B] = 
+    map(f) getOrElse None
+
 }
 
 case class Some[+A](get: A) extends Option[A]
--- fp_scala/test/src/scala/fpinscala/exceptions/OptionSpec.scala
+++ fp_scala/test/src/scala/fpinscala/exceptions/OptionSpec.scala
@@ -23,4 +23,72 @@ object OptionSpec extends SimpleTestSuite {
     
     assert(act == exp)
   }
+
+  case class Employee(name: String, department: String, manager: Option[String])
+  val employees = Seq(
+    Employee("Joe", "HR", Some("Bob")),
+    Employee("Eve", "IT", None),
+    Employee("Fred", "IT", Some("Eve")))
+
+  def convert[A](op: scala.Option[A]): fpinscala.exceptions.Option[A] = op match {
+    case scala.None => fpinscala.exceptions.None
+    case scala.Some(v) => fpinscala.exceptions.Some(v)
+  }
+  def lookupByName(name: String): Option[Employee] = convert(employees.find(_.name == name))
+
+  test("map on Option[Employee] with Some") {
+    val act = lookupByName("Joe").map(_.department)
+    val exp = Some("HR")
+
+    assert(act == exp)
+  }
+
+  test("map on Option[Employee] with None") {
+    val act = lookupByName("Joelle").map(_.department)
+    val exp = None
+
+    assert(act == exp)
+  }
+
+  test("flatMap on Option[Employee] with failing first function") {
+    val act = lookupByName("Joelle").flatMap(_.manager)
+    val exp = None
+
+    assert(act == exp)
+  }
+
+  test("flatMap on Option[Employee] with failing second function") {
+    val act = lookupByName("Eve").flatMap(_.manager)
+    val exp = None
+
+    assert(act == exp)
+  }
+
+  test("getOrElse") {
+    val act = lookupByName("Eve").map(_.department).getOrElse("Default Dept")
+    val exp = "IT"
+
+    assert(act == exp)
+  }
+
+  test("getOrElse with default") {
+    val act = lookupByName("Evelyn").map(_.manager).getOrElse("Default manager")
+    val exp = "Default manager"
+
+    assert(act == exp)
+  }
+
+  test("flatMap2 on Option[Employee] with failing first function") {
+    val act = lookupByName("Joelle").flatMap2(_.manager)
+    val exp = None
+
+    assert(act == exp)
+  }
+
+  test("flatMap2 on Option[Employee] with failing second function") {
+    val act = lookupByName("Eve").flatMap2(_.manager)
+    val exp = None
+
+    assert(act == exp)
+  }
 }
